1488427a4ffb2daceeea19278d1f69777c1e2ea4
Simhadri Kuppala <darlingmr39@gmail.com>
2025-10-04 20:30:10 +0530

    added new int.c program

--- int.c
+++ int.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main()
 {
-int a=10;
-int b=20;
-
-    printf("value of a is %d\n",a*b);
+//int a=10;
+//int b=20;
+int a;
+int b;
+    printf("enter the value of length\n");
+    scanf("%d",&a);
+    printf("enter the value of breadth\n");
+    scanf("%d",&b);
+    printf("value of a is %d\n",a*b);  
     return 0;
 }
