bb3ac0ee21929fdc849ca59f2bf4c256ae1eefea
Simhadri Kuppala <darlingmr39@gmail.com>
2025-10-31 20:26:30 +0530

    added new ifstatement.c

---
+++ ifstatement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int a=10;
+    if(a=10)
+    {
+        printf("i am 11");
+    }
+    else
+        {
+            printf("i am not 11");
+    
+        }
+return 0;
+}
